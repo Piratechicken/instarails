895edb8f419225b1e20c2ed9f5f9007dd195d305
Simbidion <simonadwyer@yahoo.com.au>
2017-10-02 12:20:21 +1100

    dunno where I am anymore...

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -7,11 +7,13 @@ class Photo < ApplicationRecord
   has_many :comments
 
   ###### For geocoder
-  geocoded_by :full_street_address   # can also be an IP address
-  after_validation :geocode          # auto-fetch coordinates
   
-  reverse_geocoded_by :latitude, :longitude
-  after_validation :reverse_geocode  # auto-fetch address
+  # geocoded_by :full_street_address   # can also be an IP address
+  # after_validation :geocode          # auto-fetch coordinates
+  
+  # reverse_geocoded_by :latitude, :longitude
+  # after_validation :reverse_geocode  # auto-fetch address
+ 
   ######
 
   def liked_by?(user)
